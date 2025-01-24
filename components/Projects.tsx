6c41dccfcd42cd98853d5ed31de7e4654e822e7b
Dusousas <dusousadeveloper@gmail.com>
2025-01-24 10:02:51 -0300

    att

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,9 +3,29 @@ import React from 'react';
 export default function Projects() {
     return (
         <>
-            <section className=''>
-                <div className=''>
-                    Projects
+            <section className='ProjectsBG py-20'>
+                <div className='maxW'>
+                    <h1 className='uppercase text-3xl font-normal font-Oswald tracking-wider text-center mb-4'>Nossos <span className='text-BlueP font-Oswald tracking-wider'>Projetos</span></h1>
+                    <p className='text-center mb-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, tempore saepe cumque explicabo ad autem atque voluptates optio deleniti neque tenetur a sit. Neque, tempore? Sapiente, minus harum. Quibusdam suscipit, accusamus sint similique dicta hic modi quasi nam nisi maiores.</p>
+
+
+                    <article className='flex flex-col items-center gap-20 w-full lg:flex-row'>
+
+                        <div className='w-1/2 flex flex-col gap-6 items-end'>
+                            <img className='w-[450px] h-[560px] object-cover' src="/slider/1.jpg" alt="" />
+                            <img className='w-[450px] h-[355px] object-cover' src="/slider/1.jpg" alt="" />
+                            <img className='w-[450px] h-[355px] object-cover' src="/slider/1.jpg" alt="" />
+                        </div>
+
+                        <div className='w-1/2 flex flex-col gap-6 items-start'>
+                            <img className='w-[450px] h-[355px] object-cover' src="/slider/2.jpg" alt="" />
+                            <img className='w-[450px] h-[355px] object-cover' src="/slider/2.jpg" alt="" />
+                            <img className='w-[450px] h-[560px] object-cover' src="/slider/2.jpg" alt="" />
+                        </div>
+
+                    </article>
+
+
                 </div>
             </section>
         </>
--- components/subc/Team.tsx
+++ components/subc/Team.tsx
@@ -4,40 +4,36 @@ import { FaInstagram } from 'react-icons/fa';
 export default function Team() {
     return (
         <>
-            <section className=''>
-                <div className=''>
-                    <h1 className='uppercase text-3xl font-normal font-Oswald tracking-wider text-center mt-10'>Nosso <span className='text-BlueP font-Oswald tracking-wider'>time</span></h1>
-
-                    <article className='py-10 flex justify-center gap-20'>
+                    <h1 className='uppercase text-3xl font-normal font-Oswald tracking-wider text-center mt-10 mb-4'>Nosso <span className='text-BlueP font-Oswald tracking-wider'>time</span></h1>
+                    <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae architecto modi nihil exercitationem voluptatem neque.</p>
+                    <article className='py-10 flex flex-wrap justify-center gap-20'>
                         {/* PROFISSSIONAL 1 */}
                         <div className=''>
-                            <div className='flex border-BlueP border'>
-                                <h1 className='vertical-text'>Lívia Urbano</h1>
-                                <img className='max-w-[350px]' src="team1.jpg" alt="" />
+                            <div className='flex border-BlueP border-l'>
+                                <h1 className='vertical-text '>Lívia Urbano</h1>
+                                <img className='w-[350px]' src="team1.jpg" alt="" />
                             </div>
                             <div className='py-2 flex justify-center gap-2'>
-                                <a className='' href=""><FaInstagram className='text-2xl' /></a>
-                                <a className='' href=""><FaInstagram className='text-2xl' /></a>
-                                <a className='' href=""><FaInstagram className='text-2xl' /></a>
+                                <a className='' href=""><FaInstagram className='text-xl' /></a>
+                                <a className='' href=""><FaInstagram className='text-xl' /></a>
+                                <a className='' href=""><FaInstagram className='text-xl' /></a>
                             </div>
                         </div>
                         {/* PROFISSSIONAL 2 */}
                         <div className=''>
-                            <div className='flex border-BlueP border'>
+                            <div className='flex border-BlueP border-l'>
                                 <h1 className='vertical-text'>Lívia Urbano</h1>
-                                <img className='max-w-[350px]' src="team1.jpg" alt="" />
+                                <img className='w-[350px]' src="team1.jpg" alt="" />
                             </div>
                             <div className='py-2 flex justify-center gap-2'>
-                                <a className='' href=""><FaInstagram className='text-2xl' /></a>
-                                <a className='' href=""><FaInstagram className='text-2xl' /></a>
-                                <a className='' href=""><FaInstagram className='text-2xl' /></a>
+                                <a className='' href=""><FaInstagram className='text-xl' /></a>
+                                <a className='' href=""><FaInstagram className='text-xl' /></a>
+                                <a className='' href=""><FaInstagram className='text-xl' /></a>
                             </div>
                         </div>
-
                     </article>
 
-                </div>
-            </section>
+    
         </>
     );
 }
